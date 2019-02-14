e600cd36507faca91e91658b1317a274aa52ddb2
KingBurst <tlcherry189@gmail.com>
2019-02-14 00:06:22 -0500

    First commit

---
+++ src/main/java/com/example/HW1/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.example.HW1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/secret")
+    public String secret() {
+        return "Oops! You found my secret!";
+    }
+}
